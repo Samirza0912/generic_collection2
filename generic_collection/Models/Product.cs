eab76c77ab358b9c608f652f45478b170d92b2df
Samirza0912 <samirza@code.edu.az>
2022-04-03 14:38:18 +0400

    feat:get start to task

---
+++ generic_collection/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace generic_collection.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+        }
+    }
+}
---
+++ generic_collection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace generic_collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
